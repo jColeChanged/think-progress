2ca7f67783a2ed6f0a94cfa5b0f07eceeac15c07
Joshua <jColeChanged@users.noreply.github.com>
2021-01-25 22:08:59 -0800

    Add small progress dataset to enable visualization
    There are several different datasets rather than one because I want to
    be able to see how the visualization behaves under several scenarios,
    but there are only three scenarios because I don't need more then that
    for initial prototypes.
    
    The lack of normalization of the total column isn't a strategic
    choice, though it may open up opportunities for visualizing progress
    over time even for known out of known projects whose scope of known is
    growing over time.
    
    This dataset has not been tested or vetted in any way aside from an
    eye check for badly colored syntax. So there might be some syntax
    errors. It would be good to setup CI to catch this sort of thing if
    the project ever grows beyond a prototype stage, but at this point it
    isn't worth the effort investment.

---
+++ src/data.js
@@ -0,0 +1,136 @@
+dataset = {
+    // Linear progression for 5 days
+    "Book One": [
+        {
+            "Created": "2021-1-1",
+            "Completed": 10,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-2",
+            "Completed": 20,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-3",
+            "Completed": 30,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-4",
+            "Completed": 40,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-5",
+            "Completed": 50,
+            "Total": 100
+        }
+    ],
+    // Linear progression for ten days
+    "Book Two": [
+      {
+            "Created": "2021-1-1",
+            "Completed": 10,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-2",
+            "Completed": 20,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-3",
+            "Completed": 30,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-4",
+            "Completed": 40,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-5",
+            "Completed": 50,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-6",
+            "Completed": 60,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-7",
+            "Completed": 70,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-8",
+            "Completed": 80,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-9",
+            "Completed": 90,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-10",
+            "Completed": 100,
+            "Total": 100
+        }
+    ],
+    // Faster performance over time
+    "Book Three": [
+        {
+            "Created": "2021-1-1",
+            "Completed": 1,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-2",
+            "Completed": 3,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-3",
+            "Completed": 7,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-4",
+            "Completed": 15,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-5",
+            "Completed": 31,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-6",
+            "Completed": 64,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-2",
+            "Completed": 20,
+            "Total": 100
+        },
+        {
+            "Created": "2021-1-3",
+            "Completed": 30,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-4",
+            "Completed": 40,
+            "Total": 100
+        }, 
+        {
+            "Created": "2021-1-5",
+            "Completed": 50,
+            "Total": 100
+        }
+    ]
+}
