ad09d346836c23d0f00a3c7b36e64fa7e4005a69
Joshua <jcolechanged@gmail.com>
2021-01-31 17:49:15 -0800

    Create progress over window function.
    _What and Why_
    
    In order to give an estimate of how long things will take we
    need an estimate of our progress over time. The simplest
    way we can get one in an automated way is to assume that our
    past progress over time and our current progress over time
    are highly correlated.
    
    Unfortunately, this isn't actually the true rate of progress.
    
    Progress can end up varying. One way we can acknowledge this is by
    conceeding that it varies over time and taking several different
    samples of progress to get a better estimate of the true progress.
    For this reason, we are creating a window function that will let us
    get several different probable progress over time metrics.
    
    We need these progress over time metrics, because with them we can
    answer questions about possible ways the progress bar might be filled.
    We also need it to answer questions about how progress is evolving.
    
    Eventually we're going to use these progress over time metrics to
    provide at a glance insight into the way progress is unfolding. Is
    progress accelerating as more work is completed? Is progress slowing
    down as we near the end of the project? What are the likely end dates
    given different rates of progress?
    
    _Testing_
    
    Validated that it gives a reasonable return via eyeballing
    the results of a console print, but this function would be
    better off being tested with a unit test.

--- src/ThinkProgress.js
+++ src/ThinkProgress.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {dataset}  from "./Data";
+import {dataset, datesetCreatedParser}  from "./Data";
 
 class ThinkProgressBarRow extends React.Component {
 
@@ -15,21 +15,63 @@ class ThinkProgressBarRow extends React.Component {
 
 class ThinkProgress extends React.Component {
 
-    /*
-    window(windowStart, windowEnd, entries) {
+    // A function which calculates the percentage of work which has been completed
+    // from the start of a window until the end of the window. The progress prior to the
+    // start of a window is the progress just before the window is reached.
+    progressOverWindow(entries, windowLength) {
+
+        // No progress over a window if the window is empty.
+        if (entries.length === 0) {
+            return 0;
+        }
+
         let currentDate = new Date();
         let nDaysAgo = new Date();
         nDaysAgo.setDate(currentDate.getDate() - windowLength);
 
-        entries.filter(function(entry) {
-           return Data.datesetCreatedParser.parse(entry.created);
+        let isInWindow = ((entry) => {
+            let date = datesetCreatedParser(entry.Created)
+            return nDaysAgo <= date && date <= currentDate;
         });
+
+        let windowStart = -1;
+        for (let i=0; i < entries.length; i++) {
+            if (isInWindow(entries[i])) {
+                windowStart = i;
+                break;
+            }
+        }
+
+        // There is no data in the window so no progress was made.
+        if (windowStart === -1) {
+            return 0;
+        }
+
+        let windowEnd = entries.length - 1;
+        for (let i=windowStart; i < entries.length; i++) {
+            if (!isInWindow(entries[i])) {
+                windowEnd = i-1;
+                break;
+            }
+        }
+
+        // At this point windowsEnd is the last entry in the window,
+        // window start is the first, and window start minus one is
+        // the entry proceeding the window. Progress is therefore
+        // window end entry progress minus the entry just before
+        // window start.
+        let initialRowIndex = windowStart - 1
+        let initiallyCompleted = initialRowIndex === 0 ? 0 : entries[initialRowIndex].Completed;
+        let finalCompleted = entries[windowEnd].Completed;
+        return finalCompleted - initiallyCompleted;
     }
-    */
+
 
     render() {
         const name = "Book One";
         const entries = dataset[name];
+        const recentProgress = this.progressOverWindow(entries, 28)
+        console.log(recentProgress);
 
         return <table>
             <thead>
