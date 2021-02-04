92097faaeda1be0a610d4dbc3ba9762928ee3871
Joshua <jcolechanged@gmail.com>
2021-02-03 20:04:59 -0800

    Render a d3 chart.
    Sets up a d3 context to do visualization in. I'm going with the pattern in which d3 is left in charge of the DOM because I like the enter, update, exit semantics of that approach.
    
    Tested by validating that I see a chart in browser.

--- src/ThinkProgress.js
+++ src/ThinkProgress.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {dataset, datesetCreatedParser}  from "./Data";
+import * as d3 from "d3";
 
 class ThinkProgressBarRow extends React.Component {
 
@@ -79,6 +80,44 @@ function progressDuringWindow(entries, window) {
 
 class ThinkProgress extends React.Component {
 
+    componentDidMount() {
+        this.drawChart();
+    }
+
+    drawChart() {
+        const data = [12, 5, 6, 6, 9, 10];
+        const w = 500;
+        const h = 500;
+        const svg = d3.select(this._rootNode)
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .style("margin-left", 100);
+
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => i * 70)
+            .attr("y", (d, i) => h - 10 * d)
+            .attr("width", 65)
+            .attr("height", (d, i) => d * 10)
+            .attr("fill", "green")
+    }
+
+    shouldComponentUpdate() {
+        // Prevents component re-rendering
+        return false;
+    }
+
+    _setRef(componentNode) {
+        this._rootNode = componentNode;
+    }
+
+    render() {
+        return <div ref={this._setRef.bind(this)} />
+    }
+    /*
     render() {
         const name = "Book One";
         const entries = dataset[name];
@@ -106,6 +145,7 @@ class ThinkProgress extends React.Component {
             </tbody>
         </table>;
     }
+     */
 }
 
 export {ThinkProgress, progressDuringWindow};
