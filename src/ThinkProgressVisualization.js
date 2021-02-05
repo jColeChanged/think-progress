c94d4b957b7b0ae1a972b65b50d291725fbf0ffd
Joshua <jcolechanged@gmail.com>
2021-02-04 19:56:57 -0800

    Switch from hardcoded chart to derived chart.

--- src/ThinkProgressVisualization.js
+++ src/ThinkProgressVisualization.js
@@ -10,7 +10,7 @@ class ThinkProgressVisualization extends React.Component {
     }
 
     drawChart() {
-        const data = [12, 5, 6, 6, 9, 10];
+        const data = dataset["Book One"].map((row) => row["Completed"] / row["Total"]);
         const w = 500;
         const h = 500;
         const svg = d3.select(this._rootNode)
