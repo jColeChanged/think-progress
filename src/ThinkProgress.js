9591e07d40d6dcc09dfbe1070112aaa62a838653
Joshua <jcolechanged@gmail.com>
2021-02-15 18:33:17 -0800

    Create extrapolation objects.
    Extrapolations should be done using the analysis objects. Currently,
    we don't use the analysis objects directly because it would couple the
    analysis object to the extrapolation object such that I would need an
    analysis object to analyse an extrapolation.
    
    This isn't tested. It is scaffolding. Future work will fill in the
    missing pieces.

--- src/ThinkProgress.js
+++ src/ThinkProgress.js
@@ -3,18 +3,25 @@ import * as React from "react";
 import {ThinkProgressTable} from "./ThinkProgressTable";
 import {ThinkProgressVisualization} from "./ThinkProgressVisualization";
 import {ThinkProgressAnalysis} from "./ThinkProgressAnalysis";
+import {ThinkProgressExtrapolation} from "./ThinkProgressExtrapolation";
 
 
 class ThinkProgress extends React.Component {
 
     render() {
-        const analysis = [
+        const analyses = [
             new ThinkProgressAnalysis(this.props.dataset, 1, "days"),
             new ThinkProgressAnalysis(this.props.dataset, 7, "days"),
             new ThinkProgressAnalysis(this.props.dataset, 30, "days"),
             new ThinkProgressAnalysis(this.props.dataset, 365, "days")
         ];
-
+        const extrapolations = analyses.map((analysis) => new ThinkProgressExtrapolation(
+            analysis.name,
+            analysis.endDate,
+            analysis.completed,
+            analysis.total,
+            analysis.progessDelta
+        ));
         return <div>
             <ThinkProgressVisualization dataset={this.props.dataset}/>
             <ThinkProgressTable dataset={this.props.dataset}/>
