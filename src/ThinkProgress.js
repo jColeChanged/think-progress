2e8ac41ef8e0e64a9b954610b5072f6e15cdba43
Joshua Cole <jcolechanged@gmail.com>
2021-02-22 19:15:33 -0800

    Ensure reasonable window length choices.
    We don't want to consider time before progress
    began as part of the estimate for how long we
    expect things to take. Filtering down the maximum
    window length under analysis to be lower than the
    number of days from the start of the progress
    tracking until the present moment is one way to
    do that.
    
    Tested that results are still the same by
    verifying visually.

--- src/ThinkProgress.js
+++ src/ThinkProgress.js
@@ -1,20 +1,26 @@
 import * as React from "react";
+import * as d3 from "d3";
 
 import {ThinkProgressTable} from "./ThinkProgressTable";
 import {ThinkProgressVisualization} from "./ThinkProgressVisualization";
 import {ThinkProgressAnalysis} from "./ThinkProgressAnalysis";
 import {ThinkProgressExtrapolation} from "./ThinkProgressExtrapolation";
+import {datesetCreatedParser} from "./ThinkProgressDataset";
 
 
 class ThinkProgress extends React.Component {
 
     render() {
-        const analyses = [
-            new ThinkProgressAnalysis(this.props.dataset, 1, "days"),
-            new ThinkProgressAnalysis(this.props.dataset, 7, "days"),
-            new ThinkProgressAnalysis(this.props.dataset, 60, "days"),
-            // new ThinkProgressAnalysis(this.props.dataset, 365, "days")
-        ];
+        let now = new Date();
+        let start = datesetCreatedParser(this.props.dataset[0].Created);
+        let numDaysSinceStart = d3.timeDay.count(start, now);
+        let windowLengths = [1, 7, 30, 365].filter((windowLength) => windowLength < numDaysSinceStart);
+        windowLengths.push(numDaysSinceStart);
+
+        const analyses = windowLengths.map(
+            (windowLength) => new ThinkProgressAnalysis(this.props.dataset, windowLength, "days")
+        );
+
         const extrapolations = analyses.map((analysis) => new ThinkProgressExtrapolation(
             analysis.name,
             analysis.endDate,
