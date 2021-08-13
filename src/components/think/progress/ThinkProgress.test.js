1735b3088485f76bcfd6335933dd82195be2db9e
Joshua Cole <jcolechanged@gmail.com>
2021-08-13 15:35:11 -0700

    Create dataset and use in test.

--- src/components/think/progress/ThinkProgress.test.js
+++ src/components/think/progress/ThinkProgress.test.js
@@ -5,15 +5,13 @@ it('test days since length calculation accuracy', () => {
     let twoDaysAgo = new Date();
     twoDaysAgo.setDate(twoDaysAgo.getDate()-2);
 
-    let entries = {
-        "entries": [{"Created": twoDaysAgo}]
-    };
-    let dataset = ThinkProgressDataset(
+    let entries = [{"Created": twoDaysAgo}];
+    let dataset = new ThinkProgressDataset(
         "Test Dataset",
         100,
         entries,
         1
     );
 
-    expect(daysSinceStart(entries)).toEqual(2);
+    expect(daysSinceStart(dataset)).toEqual(2);
 });
