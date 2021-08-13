12ba74ed26925113a2246b47e1c736620de8fd71
Joshua Cole <jcolechanged@gmail.com>
2021-08-13 14:53:53 -0700

    Convert to dataset in tests.

--- src/components/think/progress/ThinkProgress.test.js
+++ src/components/think/progress/ThinkProgress.test.js
@@ -1,4 +1,5 @@
 import {daysSinceStart} from "./ThinkProgress";
+import { ThinkProgressDataset } from "./ThinkProgressDataset";
 
 it('test days since length calculation accuracy', () => {
     let twoDaysAgo = new Date();
@@ -7,6 +8,12 @@ it('test days since length calculation accuracy', () => {
     let entries = {
         "entries": [{"Created": twoDaysAgo}]
     };
+    let dataset = ThinkProgressDataset(
+        "Test Dataset",
+        100,
+        entries,
+        1
+    );
 
     expect(daysSinceStart(entries)).toEqual(2);
 });
